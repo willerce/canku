fc4fb0fa8ae13a65179666df8ffe1b1deecf9594
zs1621 <zs1213yh@gmail.com>
2013-01-19 18:19:49 +0800

    add express.compress()

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.configure(function(){
   app.set('view engine', 'ejs');
   app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
   app.use(express.logger('dev'));
+  app.use(express.compress());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
