e3f78d466305329a3e3b470f5a9cce84c78eb462
willerce <willerce@gmail.com>
2013-01-26 14:16:21 +0800

    修复fs.exists 不存在的问题

--- routes/main.js
+++ routes/main.js
@@ -154,7 +154,7 @@ exports.shop = function (req, res, next) {
             }
           }
           //检查有没有图片菜单
-          fs.exists(path.join(__dirname, '..', 'public', 'picmenu' + req.params.id + '.jpg'), function (exists){
+          path.exists(path.join(__dirname, '..', 'public', 'picmenu' + req.params.id + '.jpg'), function (exists){
             shop.picmenu = exists ? '/picmenu' + req.params.id + '.jpg': '';
             //页面渲染
             res.render('shop', {'shop':shop, 'group':group});
